30d42fcc007974d3dd20cbf8da00eda69e1f4f9b
sohail <msohailkhan.se@gmail.com>
2023-08-29 16:34:26 +0500

    set header and side bar

--- src/common/Headers.js
+++ src/common/Headers.js
@@ -1,19 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 import connextar_logo from "../resources/images/connextar-logo-dark.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
-    <nav className="bg-gray-800 px-4 py-4">
+    <nav className="bg-gray-800 py-2">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
           {/* Logo or brand */}
@@ -21,48 +14,32 @@ const Header = () => {
             <Image src={connextar_logo} alt="logo" />
           </a>
 
-          {/* Mobile Menu Button */}
-          <button
-            className="md:hidden text-white"
-            onClick={toggleMenu}
-            aria-label="Toggle menu"
-          >
-            <FontAwesomeIcon icon={faBars} className="w-5 h-5" />
-          </button>
-
           {/* Navigation links */}
-          <div
-            className={`${
-              isMenuOpen ? "block" : "hidden"
-            } md:block md:flex space-y-2 md:space-y-0 md:space-x-4`}
-          >
-            <a
-              href="/"
-              className="text-white hover:text-gray-200 block md:inline p-2"
-            >
+          <div className="md:flex md:space-y-0 md:space-x-4 md:inline-flex">
+            <a href="/" className="text-white hover:text-gray-200 p-2">
               Home
             </a>
             <a
               href="/cx-users"
-              className="text-white hover:text-gray-200 block md:inline p-2"
+              className="text-white hover:text-gray-200 p-2 mr-2"
             >
               Users
             </a>
             <a
               href="/cx-projects"
-              className="text-white hover:text-gray-200 block md:inline p-2"
+              className="text-white hover:text-gray-200 p-2 mr-2"
             >
               Projects
             </a>
             <a
               href="/dashboard/generateInvoice"
-              className="text-white hover:text-gray-200 border border-gray-200 hover:border-gray-300 p-2 rounded-md block md:inline"
+              className="text-white hover:text-gray-200 border border-gray-200 hover:border-gray-300 p-2 mr-2 rounded-md"
             >
               Generate Invoice
             </a>
             <a
               href="api/auth/signout"
-              className="text-white hover:text-gray-200 block md:inline p-2"
+              className="text-white hover:text-gray-200 p-2"
               title="Sign Out"
             >
               <FontAwesomeIcon
--- src/common/Sidebar.js
+++ src/common/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useSession } from "next-auth/react";
 
 const Sidebar = () => {
@@ -13,39 +13,33 @@ const Sidebar = () => {
   };
 
   return (
-    <>
+    <div className="relative bg-gray-700">
       {/* Button to toggle the sidebar */}
       <button
-        className="inline cursor-pointer absolute top-15 left-2 mt-2 p-2 text-white z-10"
+        className="inline cursor-pointer absolute p-4 text-white z-10"
         onClick={toggleSidebar}
       >
-        {sidebarVisible ? (
-          <FontAwesomeIcon icon={faBars} className="mr-2" />
-        ) : (
-          <FontAwesomeIcon icon={faBars} className="mr-2 text-black" />
-        )}
+        <FontAwesomeIcon
+          icon={faBars}
+          className={sidebarVisible ? "mr-2" : "mr-2 text-black"}
+        />
       </button>
       {/* Sidebar */}
-      {sidebarVisible && (
-        <div className="px-4 py-2 bg-gray-700 lg:w-1/5">
-          <div className="my-1">
-            {/* Close button */}
-            <button
-              className="bg-gray-700 p-2 text-white absolute left-0 transform -translate-x-full z-10"
-              onClick={toggleSidebar}
-            >
-              <FontAwesomeIcon icon={faTimes} size="lg" />
-            </button>
-            <h1 className="text-2xl font-bold text-white ml-6 mr-2 mb-4">
-              Welcome {session?.user?.rbUserId}
-            </h1>
-            <h1 className="text-2xl font-bold text-white ml-6 mr-2">
-              {session?.user?.email}!
-            </h1>
-          </div>
+      <div
+        className={`${
+          sidebarVisible ? "block" : "hidden"
+        } lg:block lg:relative lg:w-1/5 lg:px-8 lg:py-2 lg:bg-gray-700`}
+      >
+        <div className="my-1">
+          <h1 className="text-2xl font-bold text-white p-3">
+            Welcome {session?.user?.rbUserId}
+          </h1>
+          <h1 className="text-2xl font-bold text-white">
+            {session?.user?.email}!
+          </h1>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 };
 
